38cb009b7920f3eaaf8cc7f0c0d92f69dba297f0
Stephani Engel <stephaniengel18@gmail.com>
2023-02-06 01:29:42 -0300

    Primeira Missão Você Programador

---
+++ script.js
@@ -0,0 +1,31 @@
+let chave = "f2b3227144ad1364f454f2d3be0c4163";
+
+function colocarNaTela(dados) {
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+}
+
+async function buscarCidade(cidade) {
+  let dados = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cidade +
+      "&appid=" +
+      chave +
+      "&lang=pt_br" +
+      "&units=metric"
+  )
+  .then(resposta => resposta.json());
+
+  colocarNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+  let cidade = document.querySelector(".input-cidade").value;
+
+  buscarCidade(cidade);
+}
